b11164aff1d8ddb1db3888524bc4af6c17a9edb9
Tohid Khan <tohidkhan9050482152@gmail.com>
2026-01-30 17:07:02 +0530

    TohidXMusic

---
+++ TohidXMusic/__init__.py
@@ -0,0 +1,30 @@
+from TohidXMusic.core.bot import Hotty
+from TohidXMusic.core.dir import dirr
+from TohidXMusic.core.git import git
+from TohidXMusic.core.userbot import Userbot
+from TohidXMusic.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Hotty()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "Systumm_music_bot"  # connect music api key "Dont change it"
